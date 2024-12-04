9a7ecb8aeafe15319895244bfb897ca993f939a5
parham ghasemi <parham.ghasemi.1388@gmail.com>
2024-12-05 02:07:04 +0330

    Added default dashboard's profile card

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -4,6 +4,7 @@ import Sidebar from "../components/Sidebar/Index"
 import { menus } from "../components/Sidebar/data";
 import Header from "../components/Header/Header";
 import clsx from "clsx";
+import { GoArrowRight } from "react-icons/go";
 
 export default function Default({ handleFullScreen, isSideBarCollapsed, setIsSideBarCollapsed }) {
   return (
@@ -16,7 +17,7 @@ export default function Default({ handleFullScreen, isSideBarCollapsed, setIsSid
         <Header onHamburgerClick={() => setIsSideBarCollapsed(prevState => !prevState)} onFullScreen={handleFullScreen} />
 
         <div className="w-full h-full p-6 gap-5 flex flex-col">
-          <div className="flex w-full justify-between pb-6">
+          <div className="flex w-full justify-between">
             <p className="text-neutral-600 font-bold">DASHBOARD</p>
             <div className="flex text-sm gap-1">
               <p className="text-neutral-900">Dashboards</p>
@@ -27,7 +28,44 @@ export default function Default({ handleFullScreen, isSideBarCollapsed, setIsSid
 
           <div className="flex w-full gap-5">
             <div className="flex flex-col w-1/3 rounded-md gap-5">
-              <div className="h-60 rounded-md bg-neutral-50"></div>
+              <div className="h-60 rounded-md bg-neutral-50 overflow-hidden">
+                <div className="w-full h-[40%] bg-slate-300 flex">
+                  <div className="h-full w-[57%] flex flex-col gap-1 p-3">
+                    <p className="text-blue-700 font-semibold text-lg">Welcome Back!</p>
+                    <p className="text-blue-700 text-sm font-normal">Skote Dashboard</p>
+                  </div>
+                  <div className="h-full w-[43%]"><img src="/profileHeader.png" className="max-w-full max-h-full" alt="" /></div>
+                  
+                </div>
+                <div className="w-full h-[60%] flex">
+                  <div className="h-full w-1/3 relative flex flex-col justify-end">
+                    <div className="rounded-full overflow-hidden bg-neutral-100 h-16 w-16 absolute -top-[15%] left-1/2 -translate-x-1/2 flex items-center justify-center">
+                    <img src="/placeHolderPfp.png" alt="" />
+                    </div>
+                    <div className="p-3 flex flex-col gap-2 h-2/3 justify-start">
+                      <p className="font-semibold text-neutral-700">Henry Pri...</p>
+                      <p className="font-medium text-sm text-neutral-500">Ui/Ux Desi...</p>
+                    </div>
+                  </div>
+
+                  <div className="h-full w-2/3 pl-4 pt-4 pr-9">
+                    <div className="flex justify-between">
+                      <div className="flex flex-col gap-2">
+                        <p className="text-neutral-700 font-semibold">124</p>
+                        <p className="text-neutral-500 text-sm">Projects</p>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <p className="text-neutral-700 font-semibold">$1245</p>
+                        <p className="text-neutral-500 text-sm">Revenue</p>
+                      </div>
+                    </div>
+                    <div className="flex items-end h-2/5">
+                      <button className="bg-sky-600 hover:bg-sky-700 transition-colors text-neutral-50 text-xs px-3 py-1.5 rounded flex items-center gap-1">View Profile <GoArrowRight /> </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
               <div className="h-72 rounded-md bg-neutral-50"></div>
             </div>
 
